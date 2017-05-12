bef8c114d635c28422266caafd470f5c0c4821eb
prachi.pawaskar <prachi.pawaskar@northgateps.com>
2017-05-12 19:07:55 +0530

    Adding new functionality and view monthly menu

--- app/src/main/java/com/tiinnamune/timesup/MainActivity.java
+++ app/src/main/java/com/tiinnamune/timesup/MainActivity.java
@@ -1,6 +1,10 @@
 package com.tiinnamune.timesup;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,13 +22,32 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Set current date on date label
+        final TextView currentDate = (TextView) findViewById(R.id.date_value);
+        String date;
+        //Set current time
+        date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+        currentDate.setText(date);
+
         //Change In time label
         changeInTimeLabel();
+
+        try {
+            //Creating table
+            db = openOrCreateDatabase("TimeDB", Context.MODE_PRIVATE, null);
+            //Day,month,year,in time,desired ou time,actual out time,total time
+            db.execSQL("CREATE TABLE IF NOT EXISTS time(day VARCHAR,month VARCHAR,year VARCHAR," +
+                    "in_time VARCHAR, desired_out_time VARCHAR, actual_out_time VARCHAR, total_time VARCHAR);");
+        }catch (Exception e){
+            System.out.println("ERROR: " + e.toString());
+        }
     }
 
     public void changeInTimeLabel(){
@@ -65,6 +88,113 @@ public class MainActivity extends AppCompatActivity {
                 actualHours.setText(totalHours());
             }
         });
+
+        //Save time
+        final Button saveButton = (Button) findViewById(R.id.save_button);
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String day = new SimpleDateFormat("dd").format(new Date());
+                String month = new SimpleDateFormat("MM").format(new Date());
+                String year = new SimpleDateFormat("yyyy").format(new Date());
+
+                System.out.println("day: " + day);
+                System.out.println("month: " + month);
+                System.out.println("year: " + year);
+                System.out.println("inTimeText: " + inTimeText.getText());
+                System.out.println("desiredTimeText: " + desiredTimeText.getText());
+                System.out.println("outTimeText: " + outTimeText.getText());
+                System.out.println("actualHours: " + actualHours.getText());
+
+                try {
+                    //Check if update already exist
+                    Cursor c1 = db.rawQuery("SELECT count("+day+") FROM time WHERE day='"+day+"'",null);
+                    if(c1.moveToFirst()) {
+                        String dateEtryFound = c1.getString(0);
+                        System.out.println("dateEtryFound SAVE button: " + dateEtryFound);
+                        if (Integer.parseInt (dateEtryFound) > 0) {
+                            showMessage("ERROR", "Record already exist");
+                        } else {
+                            //Insert the entry
+                            db.execSQL("INSERT INTO time VALUES('" + day + "','" + month + "','" + year + "','" + inTimeText.getText() + "','" +
+                                    desiredTimeText.getText() + "','" + outTimeText.getText() + "','" + actualHours.getText() + "');");
+
+                            Cursor c2 = db.rawQuery("SELECT count(" + day + ") FROM time WHERE day='" + day + "'", null);
+                            if (c2.moveToFirst()) {
+                                String entryFound = c2.getString(0);
+                                if (entryFound.isEmpty()) {
+                                    showMessage("ERROR", "Record not added");
+                                } else {
+                                    showMessage("SUCCESS", "Record added");
+                                }
+
+                            } else {
+                                showMessage("Error", "Invalid data");
+                            }
+                        }
+                    }
+                    else {
+                        showMessage("Error", "Invalid data");
+                    }
+                }catch (Exception e){
+                    System.out.println("ERROR: " + e.toString());
+                }
+
+
+            }
+        });
+
+        //Update time
+        final Button updateButton = (Button) findViewById(R.id.update_button);
+
+        updateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String day = new SimpleDateFormat("dd").format(new Date());
+                String month = new SimpleDateFormat("MM").format(new Date());
+                String year = new SimpleDateFormat("yyyy").format(new Date());
+
+                System.out.println("day: " + day);
+                System.out.println("month: " + month);
+                System.out.println("year: " + year);
+                System.out.println("inTimeText: " + inTimeText.getText());
+                System.out.println("desiredTimeText: " + desiredTimeText.getText());
+                System.out.println("outTimeText: " + outTimeText.getText());
+                System.out.println("actualHours: " + actualHours.getText());
+
+                try {
+                    //Check if update already exist
+                    Cursor c1 = db.rawQuery("SELECT count("+day+") FROM time WHERE day='"+day+"'",null);
+                    if(c1.moveToFirst()) {
+                        String dateEtryFound = c1.getString(0);
+                        System.out.println("dateEtryFound UPDATE button: " + dateEtryFound);
+
+
+//                        if (dateEtryFound != day) {
+//                            showMessage("ERROR", "Record do not exist");
+//                        } else {
+                        if(Integer.parseInt(dateEtryFound) == 1 ){
+                            //update the entry
+                            db.execSQL("UPDATE time SET day='" + day + "',month='" + month + "',year='" + year + "',in_time='" + inTimeText.getText() + "',desired_out_time='" +
+                                    desiredTimeText.getText() + "',actual_out_time='" + outTimeText.getText() + "',total_time='" + actualHours.getText() + "' WHERE day='" + day+"'");
+                            showMessage("Success", "Record updated");
+                        }
+                        else {
+                            showMessage("ERROR", "Record do not exist");
+                        }
+                    }
+                    else {
+                        showMessage("Error", "Invalid data");
+                    }
+                }catch (Exception e){
+                    System.out.println("ERROR: " + e.toString());
+                }
+
+
+            }
+        });
+
     }
 
 
@@ -150,5 +280,14 @@ public class MainActivity extends AppCompatActivity {
         //return string
         return totalHoursCompleted;
 
+    }
+
+    public void showMessage(String title,String message)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
